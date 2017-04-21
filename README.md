9e14b13e6207301bb309b2b1d09ef6906d8a0085
Marcel Dullaart <marcel.dullaart@gmail.com>
2017-04-21 13:35:54 +0200

    Added the login and its test

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# testing-angular-services
+# Testing Angular Services
+
+This project is based on the article [Testing Angular 2 HTTP Services with Jasmine](https://semaphoreci.com/community/tutorials/testing-angular-2-http-services-with-jasmine) by Soós Gábor, but updated for Angular 4.
+
+
+
--- src/app/user.service.spec.ts
+++ src/app/user.service.spec.ts
@@ -1,8 +1,8 @@
 import { inject, TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
-import { MockBackend } from '@angular/http/testing';
-import { BaseRequestOptions, Http } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { BaseRequestOptions, Http, Response, ResponseOptions } from '@angular/http';
 
 describe("UserService", () => {
 
@@ -38,8 +38,19 @@ describe("UserService", () => {
             expect(service.isLoggedIn()).toBeFalsy();
         }));
 
-    it("should send the login request to the server", (done) => {
-        done();
-    });
+    it("#login should call endpoint and return it's result", (done) => {
+        backend.connections.subscribe((connection: MockConnection) => {
+            let options = new ResponseOptions({
+                body: JSON.stringify({success: true})
+            });
+            connection.mockRespond(new Response(options));
+        });
 
+        subject
+            .login({username: "admin", password: "secret"})
+            .subscribe((response) => {
+                expect(response.json()).toEqual({success: true});
+                done();
+            })
+    });
 });
